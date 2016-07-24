916f339122007385dc4ca98cf74f4b610b1d2d97
albyh <albyheredia@gmail.com>
2016-07-23 19:22:29 -0700

    Initial commit

---
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "Saw";
+            currentProduct.ProductId = 1;
+            currentProduct.Description = "Steel Saw Blade";
+            var expected = "Hello Saw (1): Steel Saw Blade";
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SayHello_ParameterizedConstructor()
+        {
+            //Arrange
+            var currentProduct = new Product(1, "Saw", "Steel Saw Blade");
+            var expected = "Hello Saw (1): Steel Saw Blade";
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
