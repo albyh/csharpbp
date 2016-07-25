df4f83cf0b8464548f4eef2e6bc8356eb7926ed7
albyh <albyheredia@gmail.com>
2016-07-24 20:56:47 -0700

    Constants vs. Read-only fields/properties

--- AcmeApp/Acme.Biz/Product.cs
+++ AcmeApp/Acme.Biz/Product.cs
@@ -1,4 +1,5 @@
 ﻿using Acme.Common;
+using static Acme.Common.LoggingService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,17 @@ namespace Acme.Biz
     /// </summary>
     public class Product
     {
+        public const double InchesPerMeter = 39.37;
+        public readonly decimal MinimumPrice;
+
+
         #region Constructors
         public Product()
         {
+            //this.productVendor = new Vendor();
             Console.WriteLine("Product instance created");
+            //set default value for read-only field
+            this.MinimumPrice = .96m;
         }
 
         public Product(int productId,
@@ -25,12 +33,24 @@ namespace Acme.Biz
             this.ProductId = productId;
             this.ProductName = productName;
             this.Description = description;
-
+            if (this.ProductName.StartsWith("Bulk"))
+            {
+                this.MinimumPrice = 9.99m;
+            }
             Console.WriteLine("Product instance has a name: {0}", ProductName );
         }
         #endregion
 
         #region Properties
+        private DateTime? availabilityDate;
+
+        public DateTime? AvailabilityDate
+        {
+            get { return availabilityDate; }
+            set { availabilityDate = value; }
+        }
+
+
         private string productName;
 
         public string ProductName
@@ -52,18 +72,36 @@ namespace Acme.Biz
             get { return productId; }
             set { productId = value; }
         }
+
+        private Vendor productVendor;
+
+        public Vendor ProductVendor
+        {
+            get {
+                if (productVendor == null)
+                {
+                    productVendor = new Vendor();
+                }
+                return productVendor;
+                }
+            set { productVendor = value; }
+        }
+
         #endregion
 
         public string SayHello()
         {
-            var vendor = new Vendor();
-            vendor.SendWelcomeEmail("Message from Product Class");
-
+            //var vendor = new Vendor();
+            //vendor.SendWelcomeEmail("Message from Product Class");
             var emailService = new EmailService();
-            var confirmation = emailService.SendMessage("New Product", 
-                this.ProductName, )
+            var confirmation = emailService.SendMessage("New Product",
+                this.ProductName, "sales@acme.com");
 
-            return String.Format("Hello {0} ({1}): {2}", ProductName, ProductId, Description);
+            var result = LogAction("saying hello");
+
+            return String.Format("Hello {0} ({1}): {2}. Available on: {3}",
+                ProductName, ProductId, Description, AvailabilityDate?.ToShortDateString());
+
         }
     }
 }
--- AcmeApp/Tests/Acme.BizTests/ProductTests.cs
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -19,7 +19,8 @@ namespace Acme.Biz.Tests
             currentProduct.ProductName = "Saw";
             currentProduct.ProductId = 1;
             currentProduct.Description = "Steel Saw Blade";
-            var expected = "Hello Saw (1): Steel Saw Blade";
+            currentProduct.ProductVendor.CompanyName = "ABC Corp";
+            var expected = "Hello Saw (1): Steel Saw Blade. Available on: ";
 
             //Act
             var actual = currentProduct.SayHello();
@@ -33,13 +34,93 @@ namespace Acme.Biz.Tests
         {
             //Arrange
             var currentProduct = new Product(1, "Saw", "Steel Saw Blade");
-            var expected = "Hello Saw (1): Steel Saw Blade";
+            var expected = "Hello Saw (1): Steel Saw Blade. Available on: ";
 
             //Act
             var actual = currentProduct.SayHello();
 
             //Assert
             Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SayHello_ObjectInitializer()
+        {
+            //Arrange
+            var currentProduct = new Product
+            {
+                ProductId = 1,
+                ProductName = "Saw",
+                Description = "Steel Saw Blade",
+            };
+
+            var expected = "Hello Saw (1): Steel Saw Blade. Available on: ";
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void SayHello_Product_Null()
+        {
+            //Arrange
+            Product currentProduct = null;
+            var companyName = currentProduct?.ProductVendor?.CompanyName;
+
+            string expected = null;
+
+            //Act
+            var actual = companyName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ConvertMetersToInchesTest()
+        {
+            // Arrange
+            var expected = 78.74;
+
+            // Act
+            var actual = Product.InchesPerMeter * 2;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void MinimumPriceTest_Default()
+        {
+            // Arrange
+            var currentProduct = new Product();
+            var expected = .96m;
+            
+            // Act
+            var actual = currentProduct.MinimumPrice;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void MinimumPriceTest_Bulk()
+        {
+            // Arrange
+            var currentProduct = new Product(1, "Bulk Tools", "");
+            var expected = 9.99m;
+
+            // Act
+            var actual = currentProduct.MinimumPrice;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
         }
 
     }
